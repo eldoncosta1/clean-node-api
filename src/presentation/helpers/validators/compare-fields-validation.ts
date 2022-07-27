102d8a1e99e3f625957bce21a3d6a921bd89a153
Eldon Costa <eldoncosta1@gmail.com>
2022-07-27 15:45:49 -0300

    buxfix: correct compare field validation

--- src/presentation/helpers/validators/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -11,7 +11,7 @@ export class CompareFieldsValidation implements Validation {
   }
 
   validate (input: any): Error | undefined {
-    if (!input[this.fieldName] !== input[this.fieldToCompareName]) {
+    if (input[this.fieldName] !== input[this.fieldToCompareName]) {
       return new InvalidParamError(this.fieldToCompareName)
     }
   }
