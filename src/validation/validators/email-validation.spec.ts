bb90c29d522d4ec426a0bd239ce9171cb1ad9d90
Eldon Costa <eldoncosta1@gmail.com>
2022-08-03 18:20:48 -0300

    refactor: move validation to its own layer

--- src/validation/validators/email-validation.spec.ts
+++ src/validation/validators/email-validation.spec.ts
@@ -1,7 +1,7 @@
 import { EmailValidation } from './email-validation'
-import { EmailValidator } from '../../protocols/email-validator'
-import { HttpRequest } from '../../protocols'
-import { InvalidParamError } from '../../errors'
+import { EmailValidator } from '../protocols/email-validator'
+import { HttpRequest } from '../../presentation/protocols'
+import { InvalidParamError } from '../../presentation/errors'
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
