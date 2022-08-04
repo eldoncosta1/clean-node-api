975e5fa5d20f5bb57d65496adcf419e264e527a0
Eldon Costa <eldoncosta1@gmail.com>
2022-08-04 16:39:48 -0300

    feat: ensure SurveyMongoRepository add a new survey on add success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -0,0 +1,17 @@
+import { AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository'
+import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+interface AccountMongo {
+  _id: string
+  name: string
+  email: string
+  password: string
+}
+
+export class SurveyMongoRepository implements AddSurveyRepository {
+  async add (surveyData: AddSurveyModel): Promise<void> {
+    const surveyCollection = MongoHelper.getCollection('surveys')
+    await surveyCollection.insertOne(surveyData)
+  }
+}
